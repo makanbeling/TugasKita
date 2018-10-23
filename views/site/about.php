b4594cf7de622c8b051b42d92e4bd593355e6a65
I Komang Yudi Hardiyanta <14.8171@stis.ac.id>
2018-10-23 15:12:21 +0700

    Warning sweetalert

--- views/site/about.php
+++ views/site/about.php
@@ -3,13 +3,23 @@
 /* @var $this yii\web\View */
 
 use yii\helpers\Html;
+use yii2mod\alert\Alert;
 
 $this->title = 'About';
 $this->params['breadcrumbs'][] = $this->title;
 ?>
 <div class="site-about">
     <h1><?= Html::encode($this->title) ?></h1>
-
+    <?php
+    if (1==1){
+        Yii::$app->session->setFlash('', $this->title);
+    }else{
+        Yii::$app->session->setFlash('error', 'error is happened');
+    }
+    ?>
+    <?php
+    echo Alert::widget();
+    ?>
     <p>
         This is the About page. You may modify the following file to customize its content:
     </p>
